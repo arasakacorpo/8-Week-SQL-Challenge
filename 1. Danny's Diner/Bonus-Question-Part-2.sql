b24d5fad21aa43818877ccc259ce51fb4676334c
corpo <corpo@arasakamail.com>
2025-07-26 16:10:48 +0100

    Added solution for Part 2 of the bonus question

---
+++ 1. Danny's Diner/Bonus-Question-Part-2.sql
@@ -0,0 +1,21 @@
+WITH all_things AS (
+    SELECT
+    customer_id,
+    order_date,
+    product_name,
+    product_price as price,
+    join_date,
+    CASE
+        WHEN order_date >= join_date THEN 'Y' else 'N' 
+    END as member
+    FROM
+    sales
+    JOIN menu
+    USING(product_id)
+    LEFT JOIN members
+    USING(customer_id)
+)
+
+SELECT * ,
+CASE WHEN member = 'Y' THEN RANK() OVER (PARTITION BY customer_id, member ORDER BY order_date ASC)ELSE NULL END AS ranking
+FROM all_things;
